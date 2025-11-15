4ede8845f87eed71a699f88f3157302c2ae487e1
Saarthak1234 <saarthakagarwal0408@gmail.com>
2025-11-15 12:58:34 +0530

    Update appliacation-status page

--- app/(client-pages)/application-status/page.tsx
+++ app/(client-pages)/application-status/page.tsx
@@ -44,18 +44,7 @@ export default function ApplicationStatusPage() {
         {/* Top Navigation Bar */}
         <header className="flex items-center justify-between border-b bg-card px-4 sm:px-10 py-3">
           <div className="flex items-center gap-4">
-            <svg
-              className="size-6 text-primary"
-              fill="none"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"
-                fill="currentColor"
-              />
-            </svg>
-            <h2 className="text-lg font-bold tracking-tight">TalentSync HR</h2>
+            <h2 className="text-lg font-bold tracking-tight">TalentSync</h2>
           </div>
           <div className="flex flex-1 justify-end">
             <a className="text-sm font-medium text-muted-foreground hover:text-foreground" href="#">
