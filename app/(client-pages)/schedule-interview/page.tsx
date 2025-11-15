21d189bd16c1e3c7d6d000b62c87ff4f49eff5c8
Saarthak1234 <saarthakagarwal0408@gmail.com>
2025-11-15 13:50:29 +0530

    Update schedule-interview page

--- app/(client-pages)/schedule-interview/page.tsx
+++ app/(client-pages)/schedule-interview/page.tsx
@@ -48,18 +48,7 @@ export default function ScheduleInterviewPage() {
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center gap-4">
-              <svg
-                className="size-6 text-primary"
-                fill="none"
-                viewBox="0 0 48 48"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"
-                  fill="currentColor"
-                />
-              </svg>
-              <h2 className="text-lg font-bold tracking-tight">TalentSync HR</h2>
+              <h2 className="text-lg font-bold tracking-tight">TalentSync</h2>
             </div>
             <div className="flex items-center gap-4">
               <Button variant="ghost" size="icon" className="rounded-full">
